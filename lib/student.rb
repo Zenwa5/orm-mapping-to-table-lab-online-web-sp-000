97946b13048cfb8a2142b014931315d0c6644ad5
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-21 01:07:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,4 +10,11 @@ class Student
   end
   def self.create_table
     sql = <<-SQL
+
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+      grade INTEGER
+      
+    )
 end
